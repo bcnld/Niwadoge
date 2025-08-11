d2cc134f28fad597607ec7c54ce4d1e895118b59
bcnld <toki20110221@gmail.com>
2025-08-11 14:45:02 +0900

    Update title.js

--- title.js
+++ title.js
@@ -1,1 +1,4 @@
-
+document.getElementById('question-yes-btn').addEventListener('click', () => {
+  const overlay = document.getElementById('question-overlay');
+  overlay.classList.remove('active');
+});
